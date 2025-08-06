5510da05d89da5bc296df7d65384f4a0b0164da3
curryboy371 <nicecurry@gmail.com>
2025-08-06 16:53:03 +0900

    [spi] spi raw 작업중

--- myspi/my_spi.c
+++ myspi/my_spi.c
@@ -6,7 +6,6 @@
 
 #include "my_spi.h"
 
-
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/gpio/consumer.h>
@@ -15,6 +14,9 @@
 #include <linux/platform_device.h>
 #include <linux/of.h>
 #include <linux/of_gpio.h>
+
+// bitmap
+#include <linux/bitmap.h>
 
 
 /*
@@ -39,58 +41,138 @@
 #define HIGH   1
 #define LOW    0
 
-// 등록된 슬레이브 주소 저장
-static uint8_t registered_addrs[SPI_SLAVE_MAX] = { 0 };
-static int slave_count = 0;
+// bitmap 사용
 
 // SPI 구조체
-struct my_spi_data {
+typedef struct my_spi_data {
     struct gpio_desc* sclk_dec;   // 클럭
     struct gpio_desc* mosi_dec;   // 데이터 출력 (컨트롤러 → 주변장치)
     struct gpio_desc* miso_dec;   // 데이터 입력 (주변장치 → 컨트롤러)
     struct gpio_desc* cs_dec;     // 슬레이브 선택 (active-low)
 
     int ready; // 드라이버 준비 상태
-    spi_select_t selected_slave_id; // 현재 선택된 슬레이브
-    bool register_arr[SPI_SLAVE_MAX]; // slave id가 등록되었는지 여부
 
-    spi_mode_t modes[SPI_SLAVE_MAX]; // SPI 모드
+    // bitmap 사용 내부적으로 bit index로 관리
+    DECLARE_BITMAP(register_bitmap, SPI_SLAVE_MAX);
+    int slave_count;                 // 현재 등록중 슬레이브 수
+
+    spi_select_t selected_slave_id; // 현재 선택된 슬레이브
     struct mutex lock;
-}typedef my_spi_data_t;
+} my_spi_data_t;
+
 
 
 static struct my_spi_data *g_my_spi_data = NULL; // 전역 데이터 포인터
 
+// spi mode function ptr 
+//typedef void (*my_spi_mode_func_t(my_spi_data_t data, uint8_t tx_byte, uint8_t *rx_byte);
+typedef void (*my_spi_transfer_func_t)(my_spi_data_t *data, u8 tx, u8 *rx);
+
+static void my_spi_transfer_byte_mode0(my_spi_data_t* data, uint8_t tx_byte, uint8_t *rx_byte);
+static void my_spi_transfer_byte_mode1(my_spi_data_t* data, uint8_t tx_byte, uint8_t *rx_byte);
+static void my_spi_transfer_byte_mode2(my_spi_data_t* data, uint8_t tx_byte, uint8_t *rx_byte);
+static void my_spi_transfer_byte_mode3(my_spi_data_t* data, uint8_t tx_byte, uint8_t *rx_byte);
+
+static my_spi_transfer_func_t my_spi_func_arr[SPI_MODE_MAX] = {
+    [SPI_MODE_0] = my_spi_transfer_byte_mode0,
+    [SPI_MODE_1] = my_spi_transfer_byte_mode1,
+    [SPI_MODE_2] = my_spi_transfer_byte_mode2,
+    [SPI_MODE_3] = my_spi_transfer_byte_mode3,
+};
+
 // 내부 함수 전방선언
+static SPI_error_t my_spi_set_cs(spi_select_t slave_id, bool active);
 
-static SPI_error_t my_spi_set_mode(my_spi_data_t* data, my_spi_slave_data_t* slave);
-static SPI_error_t my_spi_set_cs(my_spi_data_t* data, my_spi_slave_data_t* slave, bool active);
 
-static SPI_error_t my_spi_register(my_spi_data_t* data, my_spi_slave_data_t* slave);
-static SPI_error_t my_spi_unregister(my_spi_data_t* data, my_spi_slave_data_t* slave);
+SPI_error_t my_spi_sync(my_spi_slave_data_t* slave_data, my_spi_message_t* message);
+
+static void my_spi_message_init(my_spi_message_t *m);
+static void my_spi_message_add_tail(my_spi_transfer_t* transfer, my_spi_message_t* msg);
 
 //spi_sync() / spi_async() (실제 전송)
+SPI_error_t my_spi_sync(my_spi_slave_data_t* slave_data, my_spi_message_t* message) {
 
-static SPI_error_t my_spi_sync(my_spi_data_t* data, my_spi_messgae_t* message) {
+    if(!slave_data) {
+        pr_err("my_spi_sync: slave_data is NULL\n");
+        return SPI_ERR_UNKNOWN;
+    }
+
+    if(slave_data->slave_id == SPI_SLAVE_MAX) {
+        pr_err("my_spi_sync: non select slave\n");
+        return SPI_ERR_NON_SELECT;
+    } 
+
+    if(slave_data->mode == SPI_MODE_MAX) {
+        pr_err("my_spi_sync: non select slave mode\n");
+        return SPI_ERR_NON_MODE;
+    } 
+
+    my_spi_lock(g_my_spi_data);
+
+    // CS 활성화
+    my_spi_set_cs(slave_data->slave_id, LOW);
+    udelay(SPI_DELAY_US);
+
+    // 리스트 초기화
+    my_spi_message_init(message);
 
     // 사용 예시
-    u8 tx_buf[4] = { 0x01, 0x01, 0x00, 0x00 }; // READ + ADDR + 2 dummy bytes
+    u8 tx_buf[4] = { 0x01, 0x02, 0x03, 0x04 }; // READ + ADDR + 2 dummy bytes
     u8 rx_buf[4] = { 0x00, 0x00, 0x00, 0x00 };
 
-    struct spi_transfer xfer = {
+    // spi_transfer instance
+    my_spi_transfer_t xfer = {
         .tx_buf = tx_buf,
         .rx_buf = rx_buf,
-        .len = 4,  // 4바이트로 수정
+        .len = 4,
     };
 
-    struct spi_message msg;
+    // 리스트 추가( 추가노드 xfer, 리스트 헤드msg)
+    my_spi_message_add_tail(&xfer, message);
 
-    spi_message_init(&msg);
-    spi_message_add_tail(&xfer, &msg);
-    spi_sync(dev->spi, &msg);
+    // 순회시 현재 노드를 가리킬 포인터
+    my_spi_transfer_t* xfer_iter = NULL;
+    list_for_each_entry(xfer_iter, &message->transfers, transfer_list) {
 
+        if(xfer_iter) {
+            for (size_t i = 0; i < xfer_iter->len; i++) {
+                u8 tx = xfer_iter->tx_buf ? ((u8*)xfer_iter->tx_buf)[i] : 0xFF;
+                u8 rx = 0;
+
+                // 로그 출력
+                pr_info("tx[%zu] = 0x%02X -> rx[%zu] = 0x%02X\n", i, tx, i, rx);
+
+                // 현재 모드에 맞는 transfer 함수 호출
+                my_spi_func_arr[slave_data->mode](g_my_spi_data, tx, &rx);
+
+                if (xfer_iter->rx_buf) {
+                    ((u8*)xfer_iter->rx_buf)[i] = rx;
+                }
+            }
+        }
+    }
+
+    // CS 비활성화
+    udelay(SPI_DELAY_US);
+    my_spi_set_cs(slave_data->slave_id, HIGH);
+
+    my_spi_unlock(g_my_spi_data);
+
+    return SPI_ERR_NONE;
 }
 
+
+static void my_spi_message_init(my_spi_message_t* msg) {
+
+    memset(msg, 0, sizeof(*msg));
+    INIT_LIST_HEAD(&msg->transfers);
+    INIT_LIST_HEAD(&msg->resources);
+}
+
+
+static void my_spi_message_add_tail(my_spi_transfer_t* transfer, my_spi_message_t* msg) {
+    list_add_tail(&transfer->transfer_list, &msg->transfers);
+}
 
 void my_spi_lock(struct my_spi_data* data) {
     if(data) {
@@ -104,97 +186,267 @@ void my_spi_unlock(struct my_spi_data* data) {
     }
 }
 
-static SPI_error_t my_spi_register(my_spi_data_t* data, my_spi_slave_data_t* slave) {
+void my_spi_slave_init(my_spi_slave_data_t* slave) {
+    
+    if (!slave) {
+        pr_err("my_spi_slave_init: NULL or slave\n");
+        return;
+    }
 
-    if(!data) {
-        pr_err("my_spi_register: data is NULL\n");
+    slave->slave_id = SPI_SLAVE_MAX;
+    slave->mode = SPI_MODE_MAX;
+}
+
+
+SPI_error_t my_spi_register(my_spi_slave_data_t* slave) {
+
+    if (!slave) {
+        pr_err("my_spi_register: NULL or slave\n");
         return SPI_ERR_UNKNOWN;
     }
 
-    if(!slave) {
-        pr_err("my_spi_register: slave is Null\n");
+    if (slave->slave_id != SPI_SLAVE_MAX) {
+        pr_err("my_spi_register: slave already registered (%d)\n", slave->slave_id);
         return SPI_ERR_UNKNOWN;
     }
 
-    if(slave->slave_id != SPI_SLAVE_MAX) {
-        pr_err("my_spi_register: slave id is not None %d\n", slave->slave_id);
-        return SPI_ERR_UNKNOWN;
-    }
-
-    if(slave->mode == SPI_MODE_NONE) {
-        pr_err("my_spi_register: Invalid slave selection %d\n", slave);
+    if (slave->mode == SPI_MODE_MAX) {
+        pr_err("my_spi_register: Invalid mode %d\n", slave->mode);
         return SPI_ERR_NON_MODE;
     }
 
+    mutex_lock(&g_my_spi_data->lock);
 
-    // 남은 id 부여
-    for(spi_select_t id = SPI_SLAVE_0; id <= SPI_SLAVE_MAX; ++id) {
-
-
+    // 비어있는 슬롯 검색
+    int id = find_first_zero_bit(g_my_spi_data->register_bitmap, SPI_SLAVE_MAX);
+    if (id >= SPI_SLAVE_MAX) {
+        pr_err("my_spi_register: No available slot\n");
+        mutex_unlock(&g_my_spi_data->lock);
+        return SPI_ERR_UNKNOWN;
     }
 
+    set_bit(id, g_my_spi_data->register_bitmap);
+    slave->slave_id = (spi_select_t)id;
+    g_my_spi_data->slave_count++;
 
-    slave->slave_id;
+    pr_info("my_spi_register: Registered slave id=%d mode=%d\n", id, slave->mode);
 
+    mutex_unlock(&g_my_spi_data->lock);
+    return SPI_ERR_UNKNOWN;
 }
 
-static SPI_error_t my_spi_unregister(my_spi_data_t* data, my_spi_slave_data_t* slave) {
+SPI_error_t my_spi_unregister(my_spi_slave_data_t* slave) {
 
+    if (!slave) {
+        pr_err("my_spi_unregister: NULL slave\n");
+        return SPI_ERR_UNKNOWN;
+    }
+
+    if (slave->slave_id >= SPI_SLAVE_MAX) {
+        pr_err("my_spi_unregister: Invalid slave_id=%d\n", slave->slave_id);
+        return SPI_ERR_UNKNOWN;
+    }
+
+    mutex_lock(&g_my_spi_data->lock);
+
+    if (!test_bit(slave->slave_id, g_my_spi_data->register_bitmap)) {
+        pr_err("my_spi_unregister: Slave id=%d not registered\n", slave->slave_id);
+        mutex_unlock(&g_my_spi_data->lock);
+        return SPI_ERR_UNKNOWN;
+    }
+
+    clear_bit(slave->slave_id, g_my_spi_data->register_bitmap);
+    g_my_spi_data->slave_count--;
+    pr_info("my_spi_unregister: Unregistered slave id=%d\n", slave->slave_id);
+
+    slave->slave_id = SPI_SLAVE_MAX;
+
+    mutex_unlock(&g_my_spi_data->lock);
     return SPI_ERR_NONE;
 }
 
 
-
-static SPI_error_t my_spi_set_cs(my_spi_data_t* data, my_spi_slave_data_t* slave, bool active) {
+static SPI_error_t my_spi_set_cs(spi_select_t slave_id, bool active) {
     
-    if (!data) {
+    if (!g_my_spi_data) {
         pr_err("my_spi_set_cs: data is NULL\n");
         return SPI_ERR_UNKNOWN;
     }
 
-    if(active) {
-        // CS를 LOW : Slave 선택
-        if (data->selected_slave_id == SPI_SLAVE_MAX) {
-            pr_err("my_spi_set_cs: No slave selected\n");
-            return SPI_ERR_NON_SELECT;
-        }
-        gpiod_set_value(data->cs_dec, LOW);
-        data->selected_slave_id = slave->slave_id;
+    if(slave_id == SPI_SLAVE_MAX) {
+        pr_err("my_spi_set_cs: Invalid slave ID\n");
+        return SPI_ERR_INVALID_ID;
+    }
 
-    } else {
-        // CS를 HIGH : Idle
-        gpiod_set_value(data->cs_dec, HIGH);
-        data->selected_slave_id = SPI_SLAVE_MAX;
+    gpiod_set_value(g_my_spi_data->cs_dec, active);
+
+    if(!active) {
+        g_my_spi_data->selected_slave_id = slave_id;
+    }
+    else {
+        g_my_spi_data->selected_slave_id = SPI_SLAVE_MAX;
     }
     return SPI_ERR_NONE;
 }
 
-static SPI_error_t my_spi_set_mode(my_spi_data_t* data, my_spi_slave_data_t* slave) {
+// SPI MODE 0
+static void my_spi_transfer_byte_mode0(my_spi_data_t* data, uint8_t tx_byte, uint8_t *rx_byte) {
 
-    if (!data) {
-        pr_err("my_spi_set_mode: data is NULL\n");
-        return SPI_ERR_UNKNOWN;
+    if(!data) {
+        pr_err("my_spi_gpio_send_recv_byte: data is NULL\n");
+        return;
     }
 
-    if (!slave) {
-        pr_err("my_spi_set_mode: slave is NULL\n");
-        return SPI_ERR_UNKNOWN;
+    uint8_t read = 0;
+
+    // 클럭 기본 LOW
+    // 출력 후 상승 에지에서 샘플링
+
+    // 초기 클럭상태 설정
+    gpiod_set_value(data->sclk_dec, LOW);
+
+    for (int i = 7; i >= 0; i--) {
+        // MOSI 출력 (tx bit) 
+        // MSB to LSB
+        gpiod_set_value(data->mosi_dec, (tx_byte >> i) & 0x1);
+        udelay(SPI_DELAY_US);
+
+        // CLK 상승 (샘플링은 상승 시)
+        gpiod_set_value(data->sclk_dec, HIGH);
+        udelay(SPI_DELAY_US);
+
+        // MISO 읽기
+        int bit = gpiod_get_value(data->miso_dec);
+        read |= (bit << i);
+
+        // CLK 하강
+        gpiod_set_value(data->sclk_dec, LOW);
+        udelay(SPI_DELAY_US);
     }
 
-    if (slave->mode == SPI_MODE_NONE) {
-        pr_err("my_spi_set_mode: invalid mode\n");
-        return SPI_ERR_NON_SELECT;
+    if (rx_byte)
+        *rx_byte = read;
+}
+
+// SPI MODE 1
+static void my_spi_transfer_byte_mode1(my_spi_data_t* data, uint8_t tx_byte, uint8_t *rx_byte) {
+
+    if(!data) {
+        pr_err("my_spi_gpio_send_recv_byte: data is NULL\n");
+        return;
     }
 
-    if (slave->slave_id == SPI_SLAVE_MAX) {
-        pr_err("my_spi_set_mode: invalid slave id\n");
-        return SPI_ERR_NON_MODE;
+    uint8_t read = 0;
+
+    // 클럭 기본 LOW
+    // 클럭이 한 번 움직인 후 하강 에지에서 샘플링
+
+    // 초기 클럭상태 설정
+    gpiod_set_value(data->sclk_dec, LOW);
+
+
+    // 하강 엣지 샘플링을 위해 HIGH로
+    gpiod_set_value(data->sclk_dec, HIGH);
+
+    for (int i = 7; i >= 0; i--) {
+
+        // MOSI 출력 (tx bit) 
+        // MSB to LSB
+        gpiod_set_value(data->mosi_dec, (tx_byte >> i) & 0x1);
+        udelay(SPI_DELAY_US);
+
+
+        gpiod_set_value(data->sclk_dec, HIGH);
+        udelay(SPI_DELAY_US);
+
+        // 샘플링
+        gpiod_set_value(data->sclk_dec, LOW);
+        udelay(SPI_DELAY_US);
+
+        // MISO 읽기
+        int bit = gpiod_get_value(data->miso_dec);
+        read |= (bit << i);
     }
 
-    data->modes[slave->slave_id] = slave->mode;
+    if (rx_byte)
+        *rx_byte = read;
+}
 
-    pr_debug("my_spi_set_mode: Set to mode %d slave %d\n", slave->mode, slave->slave_id);
-    return SPI_ERR_NONE;
+// SPI MODE 2
+static void my_spi_transfer_byte_mode2(my_spi_data_t* data, uint8_t tx_byte, uint8_t *rx_byte) {
+
+    if(!data) {
+        pr_err("my_spi_gpio_send_recv_byte: data is NULL\n");
+        return;
+    }
+
+    uint8_t read = 0;
+
+    // 클럭 기본 HIGH
+    // 출력 후 하강 에지에서 샘플링
+
+    // 초기 클럭상태 설정
+    gpiod_set_value(data->sclk_dec, HIGH);
+
+    
+    for (int i = 7; i >= 0; i--) {
+
+        // MOSI 출력 (tx bit) 
+        // MSB to LSB
+        gpiod_set_value(data->mosi_dec, (tx_byte >> i) & 0x1);
+        udelay(SPI_DELAY_US);
+
+        // CLK 하강 (샘플링은 하강에서)
+        gpiod_set_value(data->sclk_dec, LOW);
+        udelay(SPI_DELAY_US);
+
+        // MISO 읽기
+        int bit = gpiod_get_value(data->miso_dec);
+        read |= (bit << i);
+
+        gpiod_set_value(data->sclk_dec, HIGH);
+        udelay(SPI_DELAY_US);
+    }
+
+    if (rx_byte)
+        *rx_byte = read;
+}
+
+
+// SPI MODE 3
+static void my_spi_transfer_byte_mode3(my_spi_data_t* data, uint8_t tx_byte, uint8_t *rx_byte) {
+
+    if(!data) {
+        pr_err("my_spi_gpio_send_recv_byte: data is NULL\n");
+        return;
+    }
+
+    uint8_t read = 0;
+
+    // 클럭 기본 HIGH
+    gpiod_set_value(data->sclk_dec, HIGH);
+    udelay(SPI_DELAY_US);
+
+    // 상승 에지에서 샘플링
+
+    for (int i = 7; i >= 0; i--) {
+        
+        gpiod_set_value(data->sclk_dec, LOW);
+        udelay(SPI_DELAY_US);
+
+        gpiod_set_value(data->mosi_dec, (tx_byte >> i) & 0x1);
+        udelay(SPI_DELAY_US);
+
+        // 샘플링
+        gpiod_set_value(data->sclk_dec, HIGH);
+        udelay(SPI_DELAY_US);
+
+        int bit = gpiod_get_value(data->miso_dec);
+        read |= (bit << i);
+    }
+
+    if (rx_byte)
+        *rx_byte = read;
 }
 
 static SPI_error_t my_spi_clock_tick(my_spi_data_t* data) {
@@ -209,14 +461,8 @@ static SPI_error_t my_spi_clock_tick(my_spi_data_t* data) {
         return SPI_ERR_NON_SELECT;
     }
 
-    if(data->modes[data->selected_slave_id] == SPI_MODE_NONE) {
-        pr_err("my_spi_clock_tick: No mode set for selected slave %d\n", data->selected_slave_id);
-        return SPI_ERR_UNKNOWN;
-    }
 
-    // SPI 모드에 따라...
-    data->modes[data->selected_slave_id];
-
+    return SPI_ERR_NON_MODE;
 }
 
 // 플랫폼 드라이버 등록
@@ -258,12 +504,9 @@ static int my_spi_probe(struct platform_device *pdev) {
     }
 
     // spi 초기 값 설정
-    slave_count = 0;
-
     data->selected_slave_id = SPI_SLAVE_MAX;
+    data->slave_count = 0;
 
-    // 모든 슬레이브 모드를 초기화
-    memset(data->modes, SPI_MODE_NONE, sizeof(data->modes)); 
 
     // mutex 초기화
     mutex_init(&data->lock);
@@ -330,3 +573,8 @@ MODULE_DESCRIPTION("MY SPI Platform Device Driver");
 
 EXPORT_SYMBOL(my_spi_lock);
 EXPORT_SYMBOL(my_spi_unlock);
+EXPORT_SYMBOL(my_spi_sync);
+
+EXPORT_SYMBOL(my_spi_slave_init);
+EXPORT_SYMBOL(my_spi_register);
+EXPORT_SYMBOL(my_spi_unregister);
--- myspi/my_spi.h
+++ myspi/my_spi.h
@@ -8,61 +8,78 @@
 #include <linux/list.h>
 #include <linux/slab.h>  // kmalloc, kfree
 
+#define SPI_DELAY_US 1
+
+
 
 // SPI 에러 코드
 typedef enum {
     SPI_ERR_NONE = 0,             // 정상
+    SPI_ERR_INVALID_ID,           // 슬레이브가 ID가 유효하지 않음
     SPI_ERR_NON_SELECT,           // 슬레이브가 선택되지 않음
     SPI_ERR_NON_MODE,             // 선택되지 않은 모드
 
     SPI_ERR_UNKNOWN               // 알 수 없는 오류
 } SPI_error_t;
 
-enum spi_mode {
-    SPI_MODE_NONE = 0,  // 모드 없음 (초기값)
-    SPI_MODE_0,         // CPOL=0, CPHA=0
+typedef enum spi_mode {
+    SPI_MODE_0 = 0,     // CPOL=0, CPHA=0
     SPI_MODE_1,         // CPOL=0, CPHA=1
     SPI_MODE_2,         // CPOL=1, CPHA=0
-    SPI_MODE_3          // CPOL=1, CPHA=1
-} typedef spi_mode_t;
+    SPI_MODE_3,         // CPOL=1, CPHA=1
+    SPI_MODE_MAX       // 모드 없음 (초기값)
+} spi_mode_t;
 
-enum spi_select {
+typedef enum spi_select {
     SPI_SLAVE_0 = 0,    // 0번 슬레이브
     SPI_SLAVE_1,        // 1번 슬레이브
     SPI_SLAVE_2,        // 2번 슬레이브
 
     SPI_SLAVE_MAX       // 슬레이브 수 ( 선택되지 않은 상태)
-} typedef spi_select_t;
+} spi_select_t;
 
 // SPI Slave data 구조체
-struct my_spi_slave_data {
+typedef struct my_spi_slave_data {
     spi_mode_t mode; // slave spi mode
     spi_select_t slave_id; // slave의 id
     struct mutex lock;
-} typedef my_spi_slave_data_t;
+}my_spi_slave_data_t;
 
 
 // SPI messge
-struct my_spi_messgae {
+typedef struct my_spi_messgae {
+    // transfer들의 리스트 헤드
+    struct list_head    transfers;
 
+    /* SPI 메시지가 처리될 때 spi_res 리소스 목록 */
+    struct list_head        resources;
+    // struct spi_device   *spi;
 
-} typedef my_spi_messgae_t;
+}my_spi_message_t;
 
-struct my_spi_transfer {
+typedef struct my_spi_transfer {
 
     const void *tx_buf;
     void *rx_buf;
     unsigned int len;
 
+    // 리스트에 연결되는 노드
+    struct list_head transfer_list;
 
-
-}typedef my_spi_transfer_t;
+}my_spi_transfer_t;
 
 
 struct my_spi_data;  // 전방 선언
 
+void my_spi_slave_init(my_spi_slave_data_t* slave);
+SPI_error_t my_spi_register(my_spi_slave_data_t* slave);
+SPI_error_t my_spi_unregister(my_spi_slave_data_t* slave);
+
+
 void my_spi_lock(struct my_spi_data* data);
 void my_spi_unlock(struct my_spi_data* data);
+
+SPI_error_t my_spi_sync(my_spi_slave_data_t* slave_data, my_spi_message_t* message);
 
 #endif // __MY_SPI_H__
 
